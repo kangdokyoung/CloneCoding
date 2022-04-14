a60b2d8e2a75421c227f02d137660b1d2f93f00b
kangdokyoung <ehrud6672@naver.com>
2022-04-14 10:26:39 +0900

    이미지 슬라이드 완료

--- javascript/img_slide.js
+++ javascript/img_slide.js
@@ -5,17 +5,17 @@ let totalSlides = slides.length; //item의 갯수
 let sliderWidth = slideWapper.clientWidth; //컨테이너의 width
 let slideIndex = 0;
 let slider = document.querySelector('.slider');
-slider.stlye.width = sliderWidth * totalSlides + 'px';
+slider.style.width = sliderWidth * totalSlides + 'px';
 
 showSlides()
 
 function showSlides(){
-    for(let i;i<slides.length;i++){
-        slider.style.left = (sliderWidth * slideIndex) + 'px';
+    for(let i=0;i<slides.length;i++){
+        slider.style.left = -(sliderWidth * slideIndex) + 'px';
     }
     slideIndex++;
     if(slideIndex === totalSlides){
         slideIndex = 0;
     }
-    setTimeout(showSlides, 2000);
+    setTimeout(showSlides, 3500);
 }
