3fdc1bbc41e2e355a19d5a51bbb41495be1b477c
kangdokyoung <ehrud6672@naver.com>
2022-04-29 17:18:21 +0900

    .

--- javascript/main_popup.js
+++ javascript/main_popup.js
@@ -1,8 +1,11 @@
 $(document).ready(()=>{
     $(".circle1").click(()=>{
-        $(".popup_c1").fadeIn();
+        $(".popmenu_c1").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
     });
     $(".exit_c1").click(()=>{
-        $(".popup_c1").fadeOut();
+        $(".popmenu_c1").fadeOut();
+        $("#body").css("background-color","#fff");
+
     });
 });
