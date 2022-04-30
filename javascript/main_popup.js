30bb45f91099685a492b1eb4d22e0e0649399d56
kangdokyoung <ehrud6672@naver.com>
2022-04-30 17:42:33 +0900

    메인팝업

--- javascript/main_popup.js
+++ javascript/main_popup.js
@@ -9,3 +9,51 @@ $(document).ready(()=>{
 
     });
 });
+
+$(document).ready(()=>{
+    $(".circle2").click(()=>{
+        $(".popmenu_c2").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_c2").click(()=>{
+        $(".popmenu_c2").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
+
+$(document).ready(()=>{
+    $(".circle3").click(()=>{
+        $(".popmenu_c3").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_c3").click(()=>{
+        $(".popmenu_c3").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
+
+$(document).ready(()=>{
+    $(".square1").click(()=>{
+        $(".popmenu_s1").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_s1").click(()=>{
+        $(".popmenu_s1").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
+
+$(document).ready(()=>{
+    $(".square2").click(()=>{
+        $(".popmenu_s2").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_s2").click(()=>{
+        $(".popmenu_s2").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
