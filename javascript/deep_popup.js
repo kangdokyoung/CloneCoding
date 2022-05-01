9ef5624a58f42ef35ffc7a918761d2442ab03e73
kangdokyoung <ehrud6672@naver.com>
2022-05-01 15:11:04 +0900

    .

---
+++ javascript/deep_popup.js
@@ -0,0 +1,59 @@
+$(document).ready(()=>{
+    $(".btn1").click(()=>{
+        $(".popmenu_b1").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_b1").click(()=>{
+        $(".popmenu_b1").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
+
+$(document).ready(()=>{
+    $(".btn2").click(()=>{
+        $(".popmenu_b2").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_b2").click(()=>{
+        $(".popmenu_b2").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
+
+$(document).ready(()=>{
+    $(".btn3").click(()=>{
+        $(".popmenu_b3").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_b3").click(()=>{
+        $(".popmenu_b3").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
+
+$(document).ready(()=>{
+    $(".btn4").click(()=>{
+        $(".popmenu_b4").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_b4").click(()=>{
+        $(".popmenu_b4").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
+
+$(document).ready(()=>{
+    $(".btn5").click(()=>{
+        $(".popmenu_b5").fadeIn();
+        $("#body").css("background-color","rgba(0, 0, 0, 0.2)");
+    });
+    $(".exit_b5").click(()=>{
+        $(".popmenu_b5").fadeOut();
+        $("#body").css("background-color","#fff");
+
+    });
+});
